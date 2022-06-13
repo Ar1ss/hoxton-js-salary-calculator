f9ae6377c61a3dafe3060be839c756bed194bae4
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-06-13 22:08:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+ console.log("The salary is 500€")
+const salary = 500
+let pensionContribution = salary  * 0.05
+let total = salary - pensionContribution 
+console.log("You're salary is " + total)
+
+
+console.log("The salary is 80€")
+const salary2 = 80
+let pensionContribution2 = salary2  * 0.04
+let total2 = salary2 - pensionContribution2 
+console.log("You're salary is " + total2)
+
+
+console.log("The salary is 250€")
+const salary3 = 250
+let pensionContribution3 = salary3  * 0.08
+let total3 = salary3 - pensionContribution3 
+console.log("You're salary is " + total3)
+
+
+console.log("The salary is 450€")
+const salary4 = 450
+let pensionContribution4 = salary4  * 0.10
+let total4 = salary4 - pensionContribution4 
+console.log("You're salary is " + total4)
